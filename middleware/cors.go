7417a41b47bc063c22998b455286cf705aae4d3a
quincy51 <>
2024-10-06 22:08:03 +0800

    add jwt key

--- middleware/cors.go
+++ middleware/cors.go
@@ -12,14 +12,14 @@ func Cors() gin.HandlerFunc {
 			// 服务器支持的所有跨域请求的方法
 			c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 			// 允许跨域设置可以返回其他子段，可以自定义字段
-			c.Header("Access-Control-Allow-Headers", "authorization, origin, content-type, accept")
+			c.Header("Access-Control-Allow-Headers", "authorization, jwt, origin, content-type, accept")
 			c.Header("Allow", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
 			c.Header("Content-Type", "application/json")
 			c.Next()
 		} else {
 			c.Header("Access-Control-Allow-Origin", "*")
 			c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
-			c.Header("Access-Control-Allow-Headers", "authorization, origin, content-type, accept")
+			c.Header("Access-Control-Allow-Headers", "authorization, jwt, origin, content-type, accept")
 			c.Header("Allow", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
 			c.Header("Content-Type", "application/json")
 			// 设置缓存时间
